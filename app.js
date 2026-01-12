03c0b1d44defa4d43cefdabcb2f673c2c6368136
Anuj Sharma <sanuj0882@gmail.com>
2026-01-12 15:16:32 +0530

    ep-1

---
+++ app.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1",
+//     {id: "heading"},
+//     "Hello world"
+
+//  )
+
+const parent = React.createElement(
+    "div", {id: "parent"}, 
+    [React.createElement(
+        "div", {id: "child"},
+        [React.createElement("h1", {}, "I'm H1 tag"), React.createElement('h2', {}, "I'm h2")] ),
+    React.createElement(
+        "div", {id: "child2"},
+        [React.createElement("h1", {}, "I'm H1 tag"), React.createElement('h2', {}, "I'm h2")] )])
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
